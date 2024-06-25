00f2ab986bc0eddfd3ed7e045f1c11b965e37e30
atreyasinha <atreyavis@gmail.com>
2024-06-25 11:46:38 -0700

    refine code

--- alerts.py
+++ alerts.py
@@ -1,21 +1,30 @@
-import requests
+"""
+Module generates alerts for each of the symbols that Gemini 
+trades if the standard deviation from the hourly prices
+for past 24 hours is more than 1
+"""
 import json
 import statistics
 import datetime
+import requests
 
 
-# Function to get a list of traded symbols from the Gemini API
-# Send a GET request to the Gemini API to retrieve the list of symbols
-# Return the list of symbols in JSON format
 def get_traded_symbols():
+    """
+    Function to get a list of traded symbols from the Gemini API
+    Send a GET request to the Gemini API to retrieve the list of symbols
+    Return the list of symbols in JSON format
+    """
     response = requests.get("https://api.gemini.com/v1/symbols")
     return response.json()
 
 
-# Function to get historical price data for a given symbol
-# Send a GET request to the Gemini API to retrieve the historical price data
-# Extract the closing prices from the response
 def get_prices(symbol):
+    """
+    Function to get historical price data for a given symbol
+    Send a GET request to the Gemini API to retrieve the historical price data
+    Extract the closing prices from the response
+    """
     response = requests.get(f"https://api.gemini.com/v2/candles/{symbol}/1m")
     closing_price = []
     for p in response.json():
@@ -24,10 +33,12 @@ def get_prices(symbol):
     return closing_price
 
 
-# Function to generate an alert based on price deviation
-# Create a dictionary to store the alert data
-# Print the alert data in JSON format
 def generate_alert(symbol, deviation, last_price, average_price, price_change):
+    """
+    Function to generate an alert based on price deviation
+    Create a dictionary to store the alert data
+    Print the alert data in JSON format
+    """
     alert = {
         "timestamp": datetime.datetime.now().isoformat(),   # Current timestamp
         "log_level": "P4",                                  # Log level of the alert
@@ -46,6 +57,7 @@ def generate_alert(symbol, deviation, last_price, average_price, price_change):
 
 
 def main():
+    """Main function to process the symbols and generate alerts"""
     symbols = get_traded_symbols()
 
     for symbol in symbols:
@@ -61,7 +73,7 @@ def main():
             # Generate an alert based on the price deviation
             generate_alert(symbol, deviation, last_price,
                            average_price, price_change)
-        except Exception as e:
+        except Exception as e:  # pylint: disable=broad-exception-caught
             # Print an error message if an exception occurs
             print(f"Error processing {symbol}: {str(e)}")
 
